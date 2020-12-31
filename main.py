29d62f70085776866d31e3d458a7e7861a8c9b9b
yoruio <zeleidu@gmail.com>
2020-12-31 10:28:15 -0800

    motor correction code - ROS commented

--- main.py
+++ main.py
@@ -8,14 +8,25 @@ import cv2
 import sys
 import time
 
+''' ROS STUFF
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int8MultiArray
+from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Twist
+'''
+
 
 # setup
 
-
+# Class Fps
+# Just a simple class to calculate FPS values between frames.
 class Fps:
     prev_time = None
     curr_fps = None
     framerate_buffer = []
+
+    # smooth_fps gives fps as average of previous 10 frame times instead of instantaneous frame time
     smooth_fps = None
 
     def __init__(self, smooth=False):
@@ -23,21 +34,62 @@ class Fps:
         self.curr_fps = 0
         self.smooth = smooth
 
+    # update()
+    # function that updates frame time and returns fps
     def update(self):
         curr_time = time.time()
         self.curr_fps = int(1 / (curr_time - self.prev_time))
         self.prev_time = curr_time
-        self.framerate_buffer.append(self.curr_fps)
-        if len(self.framerate_buffer) > 10:
-            self.framerate_buffer.pop()
         if self.smooth:
+            self.framerate_buffer.append(self.curr_fps)
+            if len(self.framerate_buffer) > 10:
+                self.framerate_buffer.pop()
             self.smooth_fps = sum(self.framerate_buffer) / len(self.framerate_buffer)
             return self.smooth_fps
         else:
             return self.curr_fps
 
-    def fps(self):
-        return self.curr_fps
+    # fps()
+    # simple getter function for FPS values
+    def fps(self, smooth=None):
+        if smooth is None:
+            smooth = self.smooth
+        if smooth:
+            return self.smooth_fps
+        else:
+            return self.curr_fps
+
+
+''' ROS STUFF
+class RosPublisher(Node):
+    def __init__(self):
+        super().__init__('ros_publisher')
+        self.publisher_ = self.create_publisher(Vector3, 'drive', 10)
+
+'''
+'''
+    def send(self, data):
+        msg = Int8MultiArray()
+        msg.data = data
+        self.publisher_.publish(msg)
+        self.get_logger().info('publishing "%d"' % msg.data)
+'''
+'''
+
+    def send(self, forward, turn):
+        msg = Twist()
+        linear = Vector3()
+        linear.z = 0 - forward
+        linear.x = 0
+        linear.y = 0
+        angular = Vector3()
+        angular.y = turn
+        angular.x = 0
+        angular.z = 0
+        msg.data.linear = linear
+        msg.data.angular = angular
+        self.publisher_.publish(msg)
+'''
 
 
 def main():
@@ -49,8 +101,25 @@ def main():
 
     background_colour = white
 
+    # some settings
+    # width and height of capture and fullscreen resolution
+    (width, height) = (1280, 720)
+    # dimensions of initial bounding box
+    init_bb_dim = (100, 100)
+    # PID constant for Potential and derivative
+    (kp, kd) = (1, 1/3)
+    # This just inverts the drive numbers to match scientific axis
+    (kp, kd) = (-kp, -kd)
+
+    ''' ROS STUFF
+    print("init ROS")
+    rclpy.init()
+    ros = RosPublisher()
+    '''
+
     # screen = pygame.display.set_mode((width, height))
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_mode((width, height), pygame.FULLSCREEN)
     sw, sh = pygame.display.get_surface().get_size()
     (cw, ch) = (sw, sh)
 
@@ -65,9 +134,9 @@ def main():
     print("init video capture")
     cap = cv2.VideoCapture(0)
     print("set width")
-    cap.set(3, 1280)
+    cap.set(3, cw)
     print('set height')
-    cap.set(4, 720)
+    cap.set(4, ch)
 
     print("init misc variables")
     # initial bounding box
@@ -94,12 +163,12 @@ def main():
     }
     tracker = OPENCV_OBJECT_TRACKERS[tracker_type]()
 
-    frame_center = (cw/2, ch/2)
+    frame_center = (cw / 2, ch / 2)
     try:
 
         while True:
-            print("start loop")
             ret, orig_frame = cap.read()
+            orig_frame = cv2.flip(orig_frame, 0)
             orig_frame = cv2.resize(orig_frame, (sw, sh))
 
             frame = cv2.cvtColor(orig_frame, cv2.COLOR_BGR2RGB)
@@ -112,13 +181,11 @@ def main():
             fps_rect.topleft = (0, 0)
             screen.blit(fps_text, fps_rect)
 
-            print("draw cursor")
             # draw cursor
             if cursor_pos is not None:
-                pygame.draw.rect(screen, blue, cursor_pos + (50, 50), 3)
+                pygame.draw.rect(screen, blue, cursor_pos + init_bb_dim, 3)
                 cursor_pos = None
 
-            print("draw bounding box")
             # draw bounding box
             if bb is not None:
                 (success, box) = tracker.update(orig_frame)
@@ -126,23 +193,23 @@ def main():
                     (x, y, w, h) = [int(v) for v in box]
                     prev_bounding_box = bounding_box
                     bounding_box = (x, y, w, h)
+                    tracking_error = False
                 else:
                     tracking_error = True
 
             if bounding_box is not None:
-                if tracking_error:
+                if not tracking_error:
                     pygame.draw.rect(screen, green, bounding_box, 3)
                 else:
                     pygame.draw.rect(screen, red, bounding_box, 3)
 
-            '''
             # draw drive control
             if (bounding_box is not None) and (not tracking_error):
                 box_center = (
                     (bounding_box[0] + bounding_box[2] / 2),
                     (bounding_box[1] + bounding_box[3] / 2)
                 )
-                prev_box_center = (
+                prev_box_center = None if prev_bounding_box is None else (
                     (prev_bounding_box[0] + prev_bounding_box[2] / 2),
                     (prev_bounding_box[1] + prev_bounding_box[3] / 2)
                 )
@@ -154,18 +221,25 @@ def main():
                 p = box_center[0] - frame_center[0]
 
                 # Derivative
-                d = prev_bounding_box[0] - bounding_box[0]
+                d = 0 if prev_box_center is None else \
+                    (box_center[0] - frame_center[0]) - (prev_box_center[0] - frame_center[0])
 
-                motor_control = p + 1/3 * d
+                angular_correction = kp * p + kd * d
 
                 # print motor text
-                motor_text = comic_sans.render('motor_control: %d' % motor_control, True, green)
+                motor_text = comic_sans.render('p: %d | d: %d | angular correction: %d' % (p, d, angular_correction), True,
+                                               green)
                 motor_rect = fps_text.get_rect()
                 motor_rect.topleft = (0, 50)
                 screen.blit(motor_text, motor_rect)
+
+                ''' ROS STUFF
+                # send correction value to ROS
+                # this needs to be implemented in the ros2-rvr interface - rvr_ros.py
+                # first value - forward - should probably have this change algorithmically somehow...
+                # second value - angular
+                ros.send(100, angular_correction)
                 '''
-
-
 
             pygame.display.update()
 
@@ -173,11 +247,13 @@ def main():
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pos = pygame.mouse.get_pos()
-                    cursor_pos = (pos[0] - 25, pos[1] - 25)
+                    cursor_pos = (pos[0] - init_bb_dim[0] / 2, pos[1] - init_bb_dim[1] / 2)
+
+                # if buttons are added to the interface, add them here.
                 elif event.type == pygame.MOUSEBUTTONUP:
                     pos = pygame.mouse.get_pos()
-                    cursor_pos = (pos[0] - 25, pos[1] - 25)
-                    bb = cursor_pos + (50, 50)
+                    cursor_pos = (pos[0] - init_bb_dim[0] / 2, pos[1] - init_bb_dim[1] / 2)
+                    bb = cursor_pos + init_bb_dim
                     tracker = OPENCV_OBJECT_TRACKERS[tracker_type]()
                     tracker.init(orig_frame, bb)
 
@@ -189,7 +265,7 @@ def main():
 
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
-                cursor_pos = (pos[0] - 25, pos[1] - 25)
+                cursor_pos = (pos[0] - init_bb_dim[0] / 2, pos[1] - init_bb_dim[1] / 2)
 
     except (KeyboardInterrupt, SystemExit):
         pygame.quit()
---
+++ rvr_ros.py
@@ -0,0 +1,218 @@
+import os
+import sys
+
+import asyncio
+from sphero_sdk import SpheroRvrObserver
+from sphero_sdk import RvrStreamingServices
+
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+from std_msgs.msg import Int8MultiArray
+from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Twist
+
+
+import time
+
+import json
+
+debug = True
+
+rvr = SpheroRvrObserver()
+
+# sensor variable initialization
+imu_global = {}
+color_global = {}
+accelerometer_global = {}
+ambient_global = {}
+encoder_global = {}
+
+received = 0x00     # received byte - fully received at 0x1f
+
+# send stuff variable initialization
+
+
+# sensor publisher
+class rvrNode(Node):
+
+    def __init__(self):
+        super().__init__('sphero_node')
+        self.publisher_ = self.create_publisher(
+            String,
+            'sensors',  # publish to sensorsr channel
+            10)
+
+        self.subscription = self.create_subscription(
+            Twist,
+            'drive',   # listen on drive channel
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def send(self, message):
+        msg = String()
+        msg.data = message
+        self.publisher_.publish(msg)
+        # if debug: self.get_logger().info('Publishing: "%s"' % msg.data)
+
+    def listener_callback(self, msg):
+        if debug: print("heard")
+
+        linear = msg.data.linear
+        angular = msg.data.angular
+
+        rvr.drive_tank_normalized(
+            left_velocity=drive_data[0],    # left motor control
+            right_velocity=drive_data[1]    # right motor control
+        )
+
+        if debug: print(str(drive_data))
+
+
+rclpy.init(args=None)
+ros = rvrNode()
+
+# Check if all values are collected
+def checkData():
+    global received
+    if received == 31:
+        received = 0
+        ros.send(
+            json.dumps({
+                    **imu_global,
+                    **color_global,
+                    **accelerometer_global,
+                    **ambient_global,
+                    **encoder_global
+            })
+        )
+        #if debug: print('publish')
+
+
+
+def imu_handler(imu_data):
+    #print('IMU data response: ', imu_data)
+    #if debug: print('IMU data received')
+    global imu_global
+    global received
+    imu_global = imu_data
+    received = received | (1)
+    #print(received)
+    checkData()
+
+
+def color_detected_handler(color_detected_data):
+    #print('Color detection data response: ', color_detected_data)
+    #if debug: print('Color detection data received')
+    global color_global
+    global received
+    color_global = color_detected_data
+    received = received | (1 << 1)
+    #print(received)
+    checkData()
+
+
+def accelerometer_handler(accelerometer_data):
+    #print('Accelerometer data response: ', accelerometer_data)
+    #if debug: print('Accelerometer data received')
+    global accelerometer_global
+    global received
+    accelerometer_global = accelerometer_data
+    received = received | (1 << 2)
+    #print(received)
+    checkData()
+
+
+def ambient_light_handler(ambient_light_data):
+    #print('Ambient light data response: ', ambient_light_data)
+    #if debug: print('Ambient light data received')
+    global ambient_global
+    global received
+    ambient_global = ambient_light_data
+    received = received | (1 << 3)
+    #print(received)
+    checkData()
+
+def encoder_handler(encoder_data):
+    #print('Encoder data response: ', encoder_data)
+    #if debug: print('Encoder data received')
+    global encoder_global
+    global received
+    encoder_global = encoder_data
+    received = received | (1 << 4)
+    #print(received)
+    checkData()
+
+def will_sleep_handler():
+    if debug: print("RVR going to sleep - sending wake signal")
+    rvr.wake()
+
+def main():
+    """ This program demonstrates how to enable multiple sensors to stream.
+    """
+
+    try:
+        rvr.wake()
+
+        # Give RVR time to wake up
+        time.sleep(2)
+
+        if debug: print("Starting imu handler...")
+        rvr.sensor_control.add_sensor_data_handler(
+            service=RvrStreamingServices.imu,
+            handler=imu_handler
+        )
+        if debug: print("Starting color handler...")
+        rvr.sensor_control.add_sensor_data_handler(
+            service=RvrStreamingServices.color_detection,
+            handler=color_detected_handler
+        )
+        if debug: print("Starting accelerometer handler...")
+        rvr.sensor_control.add_sensor_data_handler(
+            service=RvrStreamingServices.accelerometer,
+            handler=accelerometer_handler
+        )
+        if debug: print("Starting ambient light handler...")
+        rvr.sensor_control.add_sensor_data_handler(
+            service=RvrStreamingServices.ambient_light,
+            handler=ambient_light_handler
+        )
+        if debug: print("Starting encoder handler...")
+        rvr.sensor_control.add_sensor_data_handler(
+            service=RvrStreamingServices.encoders,
+            handler=encoder_handler
+        )
+        if debug: print("Starting sensor control...")
+
+        #await rvr.sensor_control.start(interval=250)
+        rvr.sensor_control.start(interval=1000)
+
+        if debug: print("Starting keep awake...")
+        rvr.on_will_sleep_notify(will_sleep_handler)
+
+        #TODO: Battery state handler
+
+
+        #TODO:
+
+        while True:
+            rclpy.spin_once(ros)
+
+
+    except KeyboardInterrupt:
+        print('\nProgram terminated with keyboard interrupt.')
+
+    finally:
+        rvr.sensor_control.clear()
+
+        # Delay to allow RVR issue command before closing
+        time.sleep(.5)
+
+        rvr.close()
+    # The asyncio loop will run forever to allow infinite streaming.
+
+
+if __name__ == '__main__':
+    main()
